1457e63f370c9f5c57d0736d1b0adca0ed5524c2
Valentin Knabel <dev@vknabel.com>
2017-11-07 22:04:10 +0100

    [fix] Shim for XCTAssertNoThrow

---
+++ Tests/FiniteTests/SwiftShim.swift
@@ -0,0 +1,35 @@
+//
+//  SwiftShim.swift
+//  FiniteTests
+//
+//  Created by Valentin Knabel on 07.11.17.
+//
+
+import XCTest
+
+#if os(macOS)
+    public typealias XCTestCaseClosure = (XCTestCase) throws -> Void
+    public typealias XCTestCaseEntry = (testCaseClass: XCTestCase.Type, allTests: [(String, XCTestCaseClosure)])
+
+    public func testCase<T: XCTestCase>(_ allTests: [(String, (T) -> () -> Void)]) -> XCTestCaseEntry {
+        return (T.self, [])
+    }
+#endif
+
+public func allTests() -> [XCTestCaseEntry] {
+    return [
+        testCase(TransitionTests.allTests),
+        testCase(StateFlowTests.allTests),
+        testCase(StateMachineTests.allTests),
+    ]
+}
+
+#if !swift(>=4.0)
+    func XCTAssertNoThrow<T>(_ expression: @autoclosure () throws -> T, _ message: @autoclosure () -> String = "XCTAssertNoThrow", file: StaticString = #file, line: UInt = #line) {
+        do {
+            _ = try expression()
+        } catch {
+            XCTFail(message(), file: file, line: line)
+        }
+    }
+#endif
